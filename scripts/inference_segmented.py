e37412637e845afcf5d475f236b4681c48023fcf
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-04-18 01:27:48 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -68,3 +68,4 @@ def get_video_duration(video_path: str) -> float:
             if video_stream.duration:
                 return float(video_stream.duration * video_stream.time_base)
             else:
+                print("Warning: Could not determine video duration from metadata.")
