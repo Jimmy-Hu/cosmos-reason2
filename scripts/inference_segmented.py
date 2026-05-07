96930958a9da2124d01083085624a3736f227677
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-08 05:13:09 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -88,3 +88,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
         List[Image.Image]: List of PIL images for this segment.
     """
     frames = []
+    try:
