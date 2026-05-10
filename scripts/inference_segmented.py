abc8c0137b15312add99dd2c4975e1a62fc88e7a
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-11 01:08:58 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -91,3 +91,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
     try:
         print("    [Debug] frame_generator: Opening video container...")
         with av.open(video_path) as container:
+            stream = container.streams.video[0]
