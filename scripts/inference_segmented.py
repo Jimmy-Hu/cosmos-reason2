e9948d195b88f1bf4554785322fbf98cbf37a80a
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-19 00:29:16 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -99,3 +99,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
             
             # Calculate the time base interval for seeking
             seek_target = int(start_sec / stream.time_base)
+            
