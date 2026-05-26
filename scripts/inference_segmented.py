cf7baf5720039caebf1b608abfab6b6b3b25bbf9
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-26 08:14:40 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -106,3 +106,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
             print("    [Debug] frame_generator: Seek completed. Starting to decode frames...")
             
             last_captured_time = -1.0
+            min_interval = 1.0 / fps
