628ec670e6cf08449ead36c92f6cbd30a0bcd863
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-06-04 08:46:22 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -115,3 +115,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
 
                 if frame_count % 300 == 0:
                      print(f"    [Debug] frame_generator: Still decoding... Current frame time is {current_time:.2f}s")
+
