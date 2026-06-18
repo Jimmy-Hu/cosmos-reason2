9ad588280453291c1599f4113c1a20d22985c453
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-06-18 08:18:41 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -129,3 +129,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
                 if last_captured_time < 0 or (current_time - last_captured_time) >= min_interval:
                     pil_image = frame.to_image()
                     frames.append(pil_image)
+                    last_captured_time = current_time
