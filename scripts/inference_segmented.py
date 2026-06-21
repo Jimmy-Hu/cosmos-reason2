512a9ff2d12ed23447b104362b33df5213328e2f
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-06-21 23:07:57 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -132,3 +132,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
                     last_captured_time = current_time
                     
             print(f"    [Debug] frame_generator: Successfully finished decoding sequence.")
+    except Exception as e:
