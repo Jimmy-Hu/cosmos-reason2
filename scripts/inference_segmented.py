899e2c0a832a44ab5bee903371115aaad91f2ecf
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-06-24 00:10:40 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -135,3 +135,4 @@ def frame_generator(video_path: str, start_sec: float, end_sec: float, fps: int)
     except Exception as e:
         print(f"    [Debug] frame_generator Error: {e}")
         
+    return frames
