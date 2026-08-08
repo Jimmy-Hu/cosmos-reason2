45161e8ddbcc7cd9474e4d31aba45545003e7f71
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-08-08 14:15:57 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -157,6 +157,7 @@ def main():
     parser.add_argument(
         "--video_path", 
         type=str, 
+                del generated_ids
             if 'generated_ids_trimmed' in locals():
                 del generated_ids_trimmed
             del segment_frames
