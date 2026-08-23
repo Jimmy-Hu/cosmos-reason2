4a7b97b10669eb027287924e28a1475cee661c81
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-08-23 22:21:18 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -159,6 +159,7 @@ def main():
     parser.add_argument(
         "--video_path", 
         type=str, 
+                f_out.write(f"### Segment {chunk_index + 1} ({current_time:.1f}s - {end_time:.1f}s)\n")
                 f_out.write(output_text + "\n\n")
                 f_out.flush() 
 
