8d969bd03130c069d6b589be622ed3e29333e5fa
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-09-09 07:33:15 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -159,6 +159,7 @@ def main():
     parser.add_argument(
         "--video_path", 
         type=str, 
+                    )
                 
                 print("    [Debug] Decoding generated tokens...")
                 generated_ids_trimmed = [
