ab04523ac5e4815b3778f0ab8ce417abf93ab8c2
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-09-18 13:13:29 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -159,6 +159,7 @@ def main():
     parser.add_argument(
         "--video_path", 
         type=str, 
+                max_new_tokens = 512
                 streamer = ProgressStreamer(max_new_tokens)
                 
                 with torch.no_grad():
