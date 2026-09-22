2a08f067048be924b68f2965f4bcf9aa8e58fa5e
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-09-23 01:02:43 +0800

    Update inference_segmented.py
    Update inference_segmented.py

--- scripts/inference_segmented.py
+++ scripts/inference_segmented.py
@@ -159,6 +159,7 @@ def main():
     parser.add_argument(
         "--video_path", 
         type=str, 
+                inputs = inputs.to(model.device)
 
                 print("    [Debug] Starting model generation (this may take a while)...")
                 
